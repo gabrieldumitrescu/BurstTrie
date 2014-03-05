7b8c5a52a31cf3c57ec6100e07c5e0ec24b30189
Gabriel Dumitrescu <dumigab77@gmail.com>
2014-03-05 20:11:16 +0200

    Cosmetic changes to the testapp.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX=g++
-CXXFLAGS=-g -Wall
+CXXFLAGS=-g -Wall -O0
 
 APP=testapp
 SRC=testapp.cpp BurstTrie.cpp record.cpp
--- testapp.cpp
+++ testapp.cpp
@@ -5,37 +5,44 @@
 #include "BurstTrie.h"
 #include <fstream>
 
+bool openInFile(const std::string &filename, std::ifstream &file){
+  file.open(filename.c_str(),std::ifstream::in);
+  if(!file.is_open()) {
+    printf("Could not open input file %s", filename.c_str());
+    return false;
+  }
+  return true;
+}
+
 int main(int argc, char* argv[]){
   BurstTrie bt;
   std::string fileName="eng_com.dic";
-  std::ifstream dictFile(fileName.c_str(),std::ifstream::in);
-  if(!dictFile.is_open()) {
-    printf("Could not open input file %s", fileName.c_str());
-    return 1;
+  std::ifstream dictFile;
+  if(openInFile(fileName,dictFile)){
+    int lineNo=0;
+    std::string line;
+    while(getline(dictFile,line)){
+      lineNo++;
+      //printf("Adding line no %d\n",lineNo);
+      bt.addRecord(Record(line));
+    }
+    printf("Added %d lines\n",lineNo);
+    dictFile.close();
+    fileName="toSearch.dic";
+    if(openInFile(fileName,dictFile)){
+      int found=0;
+      int notFound=0;
+      while(getline(dictFile,line)){
+	if(bt.findRecord(Record(line)))
+	  //printf("Found record %s\n",line.c_str());
+	  found++;
+	else
+	  notFound++;
+	//printf("Record %s not found\n",line.c_str());
+      }
+      printf("Found %d records not found %d records.\n",found,notFound);
+      dictFile.close();
+    }
   }
-
-  int lineNo=0;
-  std::string line;
-  while(getline(dictFile,line)){
-    lineNo++;
-    //printf("Adding line no %d\n",lineNo);
-    bt.addRecord(Record(line));
-  }
-  printf("Added %d lines\n",lineNo);
-  dictFile.close();
-  fileName="toSearch.dic";
-  dictFile.open(fileName.c_str(),std::ifstream::in);
-  int found=0;
-  int notFound=0;
-  while(getline(dictFile,line)){
-    if(bt.findRecord(Record(line)))
-      //printf("Found record %s\n",line.c_str());
-      found++;
-    else
-      notFound++;
-    //printf("Record %s not found\n",line.c_str());
-  }
-  printf("Found %d records not found %d records.\n",found,notFound);
-  dictFile.close();
   return 0;
 }
