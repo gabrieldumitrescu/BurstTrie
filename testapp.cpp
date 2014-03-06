1d84b428047eefd1d91bcf9c7f6dba4f65a00c3d
Gabriel Dumitrescu <dumigab77@gmail.com>
2014-03-06 15:24:53 +0200

    Added comparison with a hash table -- standard unordered_map.

--- testapp.cpp
+++ testapp.cpp
@@ -4,6 +4,7 @@
 #include "record.h"
 #include "BurstTrie.h"
 #include <fstream>
+#include <unordered_map>
 
 bool openInFile(const std::string &filename, std::ifstream &file){
   file.open(filename.c_str(),std::ifstream::in);
@@ -14,11 +15,10 @@ bool openInFile(const std::string &filename, std::ifstream &file){
   return true;
 }
 
-int main(int argc, char* argv[]){
+void testBurstTrie(const std::string& dictFileName, const std::string &searchFileName){
   BurstTrie bt;
-  std::string fileName="eng_com.dic";
   std::ifstream dictFile;
-  if(openInFile(fileName,dictFile)){
+  if(openInFile(dictFileName,dictFile)){
     int lineNo=0;
     std::string line;
     while(getline(dictFile,line)){
@@ -28,8 +28,7 @@ int main(int argc, char* argv[]){
     }
     printf("Added %d lines\n",lineNo);
     dictFile.close();
-    fileName="toSearch.dic";
-    if(openInFile(fileName,dictFile)){
+    if(openInFile(searchFileName,dictFile)){
       int found=0;
       int notFound=0;
       while(getline(dictFile,line)){
@@ -44,5 +43,45 @@ int main(int argc, char* argv[]){
       dictFile.close();
     }
   }
+}
+
+void testHashMap(const std::string& dictFileName, const std::string &searchFileName){
+  std::unordered_map<std::string,std::string> map;
+  std::ifstream dictFile;
+  if(openInFile(dictFileName,dictFile)){
+    int lineNo=0;
+    std::string line;
+    while(getline(dictFile,line)){
+      lineNo++;
+      //printf("Adding line no %d\n",lineNo);
+      std::pair<std::string,std::string> record (line,line);
+      map.insert(record);
+    }
+    printf("Added %d lines\n",lineNo);
+    dictFile.close();
+    if(openInFile(searchFileName,dictFile)){
+      int found=0;
+      int notFound=0;
+      while(getline(dictFile,line)){
+	if(map.find(line)==map.end())
+	  //printf("Found record %s\n",line.c_str());
+	  notFound++;
+	else
+	  found++;
+	//printf("Record %s not found\n",line.c_str());
+      }
+      printf("Found %d records not found %d records.\n",found,notFound);
+      dictFile.close();
+    }
+  }
+}
+
+
+int main(int argc, char* argv[]){
+  std::string dictFileName="eng_com.dic";
+  std::string srchFileName="toSearch.dic";
+  //testBurstTrie(dictFileName,srchFileName);
+  testHashMap(dictFileName,srchFileName);
+
   return 0;
 }
